1d88c9ccfe6733909bcc61d2f792454f5cb5d205
Timur Feix <tmfx40@gmail.com>
2022-04-18 23:34:26 +0800

    Outline Structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="main">
+      <div className="loginCont">
+        <div className="smallText">
+          If you do not have an account, you can 
+          <span className="blueColor"> Register</span>, or login
+          as a <span className="blueColor">Demo User</span>.
+        </div>
+      </div>
+      <div className="loginSide"></div>
     </div>
   );
 }
