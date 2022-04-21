13e2e87d3aad2940dcbc0c8324df6df4df38aace
Timur Feix <tmfx40@gmail.com>
2022-04-21 23:08:39 +0800

    worked on backend init a bit

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extendet: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extendet: true }));
+app.use(cors());
+
+const CONNECTION_URL = '/mongodb+srv://morbidly-annoying-scotch-tape:6Pq4ZQvzZvdisgFY@cluster0.ggphy.mongodb.net/cluster0?retryWrites=true&w=majority';
+
+const PORT = process.env.port || 5000; 
